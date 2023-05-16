aa37d8fce7426f0637b22e29d4923a8537b56fe6
soroushfathi <soroush8fathi@gmail.com>
2023-05-16 23:37:21 +0330

    initial forward_checking function, and use it in backtracking

--- CSP/Solver.py
+++ CSP/Solver.py
@@ -47,16 +47,30 @@ class Solver:
             uvar.value = uval
             print(f"unass var: {uvar}")
             if self.is_consistent(uvar):
+                self.forward_check(uvar)
                 result = self.backtracking()
                 if result:
                     return result
+                else:
+                    pass # todo: unforward_check 
             else:
                 uvar.value = None
         return False
 
     def forward_check(self, var):
-        pass
-        # Write your code here
+        pre_var_value = var.value
+        self.problem.calculate_neighbors()
+        variable_value = var.value
+
+        # set the value for variable, so other domains will be remove
+        var.domain = [variable_value]
+        # remove the value from all the neighbors, if it has more than one value, then remove the value
+        for neighbor in enumerate(var.neighbors):
+            if len(neighbor.domain) > 1:
+                neighbor.domain.remove(variable_value)
+            else:
+                # todo: revert the changed values from neighbors
+                break
 
     def select_unassigned_variable(self) -> Optional[Variable]:
         if self.use_mrv:
@@ -70,8 +84,7 @@ class Solver:
         return var.domain
 
     def mrv(self) -> Optional[Variable]:
-        pass
-        # Write your code here
+        unassigned_variables = self.problem.get_unassigned_variables()
 
     def is_consistent(self, var: Variable):
         print("is consistant: ".format([x.is_satisfied() for x in self.problem.constraints]))

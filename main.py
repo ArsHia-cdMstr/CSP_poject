551f3304d6881d03c43e2899160b2e2bcb15e410
soroushfathi <soroush8fathi@gmail.com>
2023-05-18 09:33:52 +0330

    testing

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from SecretSanta.SecretSantaProblem import SecretSantaProblem
 from States.StatesProblem import StatesProblem
 from Sudoku.SudokuProblem import SudokuProblem
 
-#
-# states = StatesProblem()
-# s = Solver(states, use_forward_check=True)
-# s.solve()
-# states.print_assignments()
-#
-#
-#secret_santa = SecretSantaProblem(['arman', 'alice', 'nader', 'bob', 'sarah', 'iman'])
+
+states = StatesProblem()
+s = Solver(states, use_forward_check=True)
+s.solve()
+states.print_assignments()
+
+
+# secret_santa = SecretSantaProblem(['arman', 'alice', 'nader', 'bob', 'sarah', 'iman'])
 # secret_santa = SecretSantaProblem([str(i) for i in range(100)])
 # # secret_santa.assign_givers_and_receivers()
 # s = Solver(secret_santa)
@@ -46,22 +46,22 @@ from Sudoku.SudokuProblem import SudokuProblem
 #     [7, 0, 3, 0, 1, 8, 0, 0, 0]
 # ]
 #
-grid2 = [
-    [0, 0, 3, 0, 2, 0, 6, 0, 0],
-    [9, 0, 0, 3, 0, 5, 0, 0, 1],
-    [0, 0, 1, 8, 0, 6, 4, 0, 0],
+# grid2 = [
+#     [0, 0, 3, 0, 2, 0, 6, 0, 0],
+#     [9, 0, 0, 3, 0, 5, 0, 0, 1],
+#     [0, 0, 1, 8, 0, 6, 4, 0, 0],
 
-    [0, 0, 8, 1, 0, 2, 9, 0, 0],
-    [7, 0, 0, 0, 0, 0, 0, 0, 8],
-    [0, 0, 6, 7, 0, 8, 2, 0, 0],
+#     [0, 0, 8, 1, 0, 2, 9, 0, 0],
+#     [7, 0, 0, 0, 0, 0, 0, 0, 8],
+#     [0, 0, 6, 7, 0, 8, 2, 0, 0],
 
-    [0, 0, 2, 6, 0, 9, 5, 0, 0],
-    [8, 0, 0, 2, 0, 3, 0, 0, 9],
-    [0, 0, 5, 0, 1, 0, 3, 0, 0]
-]
+#     [0, 0, 2, 6, 0, 9, 5, 0, 0],
+#     [8, 0, 0, 2, 0, 3, 0, 0, 9],
+#     [0, 0, 5, 0, 1, 0, 3, 0, 0]
+# ]
 
-#
-sudoku = SudokuProblem(grid2)
-s = Solver(sudoku, use_mrv= True, use_forward_check=True)
-s.solve()
-sudoku.print_assignments()
+
+# sudoku = SudokuProblem(grid2)
+# s = Solver(sudoku, use_mrv= True, use_forward_check=True)
+# s.solve()
+# sudoku.print_assignments()
